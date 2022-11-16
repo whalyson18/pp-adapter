a08d9e314aade5a89214b851064c2003265cab54
Whalyson Ramos <51969850+whalyson18@users.noreply.github.com>
2022-11-16 20:55:41 -0300

    Add files via upload

--- src/main/java/Principal2.java
+++ src/main/java/Principal2.java
@@ -1,7 +1,16 @@
 public class Principal2{
     public static void main(String[] args) {
-        System.out.println(new AlphaVantageAdapter().getCotacao("INTL"));
-        System.out.println(new YahooAdapter().getCotacao("MGLU3.SA"));
-        System.out.println(new QuandlAdapter().getCotacao("WIKI/AAPL"));
+
+        ServicoCotacao cotacao = new YahooAdapter();
+        System.out.println(cotacao.getCotacao("CMIG4.SA"));
+
+        cotacao = new AlphaVantageAdapter();
+        System.out.println(cotacao.getCotacao("INTL"));
+
+        cotacao = new QuandlAdapter();
+        System.out.println(cotacao.getCotacao("WIKI/AAPL"));
+        //System.out.println(new AlphaVantageAdapter().getCotacao("INTL"));
+        //System.out.println(new YahooAdapter().getCotacao("MGLU3.SA"));
+        //System.out.println(new QuandlAdapter().getCotacao("WIKI/AAPL"));
     }
 }
